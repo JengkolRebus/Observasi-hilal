ac42060198c36a9181115b7a3ac0e5114140451d
_jengkolrebus <64493984+JengkolRebus@users.noreply.github.com>
2020-10-12 00:38:23 +0700

    Update observehilal.py
    mengubah format output date dari Calendar().
    
    menambahkan fungsi menghitung waktu hilal dengan compute().

--- observehilal.py
+++ observehilal.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkcalendar import *
 from datetime import datetime
+import compute
 
 class var():
     dari = ""
@@ -24,7 +25,11 @@ in_long.grid(row=1, column=1)
 
 # Datepicker dari
 label_dari = tk.Label(text='Dari').grid(row=3, column=0)
-cal_dari = Calendar(window, selectmode="day", year=datetime.today().year, month=datetime.today().month, day=datetime.today().day)
+cal_dari = Calendar(window, selectmode="day",
+                    date_pattern='y/mm/d',
+                    year=datetime.today().year,
+                    month=datetime.today().month,
+                    day=datetime.today().day)
 cal_dari.grid(row=3, column=1)
 def Grab_dari():
     var.dari = cal_dari.get_date()
@@ -34,7 +39,11 @@ button_dari.grid(row=4, column=1)
 
 # Datepicker Sampai
 label_sampai = tk.Label(text='Sampai').grid(row=3, column=2)
-cal_sampai = Calendar(window, selectmode="day", year=datetime.today().year, month=datetime.today().month+1, day=datetime.today().day)
+cal_sampai = Calendar(window, selectmode="day",
+                    date_pattern='y/mm/d',
+                    year=datetime.today().year,
+                    month=datetime.today().month+1,
+                    day=datetime.today().day)
 cal_sampai.grid(row=3, column=3)
 def Grab_sampai():
     var.sampai = cal_sampai.get_date()
@@ -44,14 +53,22 @@ button_sampai.grid(row=4, column=3)
 
 
 # Jalankan program
-def compute():
+def Get_param():
+    lat = val_lat.get()
+    long = val_long.get()
+    dari = var.dari.split('/')
+    sampai = var.sampai.split('/')
+    dari = [int(i) for i in dari]
+    sampai = [int(i) for i in sampai]
     print("Parameter:")
-    print(f"\tLatitude: {val_lat.get()}")
-    print(f"\tLongitude: {val_long.get()}")
-    print(f"\tDari: {var.dari}")
-    print(f"\tSampai: {var.sampai}")
+    print(f"\tLatitude: {lat}")
+    print(f"\tLongitude: {long}")
+    print(f"\tDari: {dari}")
+    print(f"\tSampai: {sampai}")
+    compute.result(lat, long, dari, sampai)
+    
 
-button_hitung = Button(window, text="Hitung", command=compute)
+button_hitung = Button(window, text="Hitung", command=Get_param)
 button_hitung.grid(row=5, column=0)
 
 window.mainloop()
