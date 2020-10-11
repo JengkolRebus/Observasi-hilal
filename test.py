327258f2bfb0f5d0ea5b3b69a088351023a409c5
_jengkolrebus <64493984+JengkolRebus@users.noreply.github.com>
2020-10-12 00:59:08 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 from datetime import datetime as d
+from tkinter import *
+from pandastable import Table
 
 date = d.today().year
 
 print(date)
+
+window = Tk()
+f1 = Frame(window)
+f2 = Frame(window)
+
+pt = Table(f1)
+pt.show()
+
+window.mainloop()
