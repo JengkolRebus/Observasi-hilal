af90bb4ed196eabc154e6e6091b9a485d9f5e84b
_jengkolrebus <64493984+JengkolRebus@users.noreply.github.com>
2021-01-07 20:49:26 +0700

    Update DMStoDecimal.py

--- test/DMStoDecimal.py
+++ test/DMStoDecimal.py
@@ -9,10 +9,11 @@ result = 'data_dmstodec.csv'
 with open(file_name) as data:
     for line in data:
         val = line.split('\t')
-        alt = val[0]
-        az = val[1]
-        alt = alt.split(':')
-        alt = int(alt[0]) + (int(alt[1])/60) + (int(alt[2])/3600)
-        az = az.split(':')
-        az = int(az[0]) + (int(az[1])/60) + (int(az[2])/3600)
-        print(round(alt, 4), round(az, 4))
+        Sf = val[0]
+        PP = val[1]
+        Sf = Sf.split(':')
+        Sf = int(Sf[0]) + (int(Sf[1])/60) + (float(Sf[2])/3600)
+        PP = PP.split(':')
+        PP = int(PP[0]) + (int(PP[1])/60) + (float(PP[2])/3600)
+        selisih = PP-Sf
+        print(round(Sf, 4), round(PP, 4), round(selisih, 4))
